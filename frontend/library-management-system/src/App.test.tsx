9f19fb62ca8f604f999b8ad14809b90042c5e63f
mciesla <miloszc009@gmail.com>
2024-11-21 13:31:17 +0100

    KAN-12 fixed Vitest

--- frontend/library-management-system/src/App.test.tsx
+++ frontend/library-management-system/src/App.test.tsx
@@ -1,14 +1,13 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import App from './App';
 
-test('increments count when button is clicked', async () => {
+test('Fetch data', async () => {
   render(<App />);
 
-  const buttonElement = screen.getByRole('button', { name: /fetch data/i });
+  const buttonElement = screen.getByRole('button', { name: 'fetch data' });
 
   fireEvent.click(buttonElement);
 
   const fetchedData = await screen.findByTestId('fetched-data');
-  expect(fetchedData).toHaveTextContent('Hello World');
+  expect(fetchedData.textContent).toContain('Hello World');
 });
