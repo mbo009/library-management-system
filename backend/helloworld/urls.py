65c96d6368c7b05e38506ae67d00c348ba2f3165
atarsander <szymczyk.alek1@gmail.com>
2025-01-14 13:35:22 +0100

    Rename of backend folder from helloworld to api

--- backend/helloworld/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('hello_world/', views.hello_world_endpoint, name='hello_world'),
-    path('produce/', views.produce_message, name='produce_message'),
-    path('consume/', views.consume_message, name='consume_message')
-]
-
