e782e7bf6123ae24c9fff7aca84f351512b914b1
joszker <joszko.czarnecki@gmail.com>
2025-01-14 20:25:47 +0100

    Added list, detail api views for book, author

--- backend/helloworld/serializers.py
+++ backend/helloworld/serializers.py
@@ -2,13 +2,16 @@ from rest_framework import serializers
 from .models import Book, Author
 
 
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = "__all__"
-
-
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
+        fields = "__all__"
+
+
+
+class BookSerializer(serializers.ModelSerializer):
+    authors = AuthorSerializer(many=True)
+    
+    class Meta:
+        model = Book
         fields = "__all__"
--- backend/helloworld/urls.py
+++ backend/helloworld/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("find_book/", views.find_book, name="find_book"),
     path("sign_in/", views.sign_in, name="sign_in"),
     path("sign_up/", views.sign_up, name="sign_up"),
+    path('books', views.BookListView.as_view(), name='book_list'),
+    path('authors', views.AuthorListView.as_view(), name='author_list'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book_detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author_detail')
 ]
